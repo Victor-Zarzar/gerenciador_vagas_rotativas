c8b25ede5ce88b33ddfc28214c5f3a51ffb3e2af
victorzarzar <victorzarzar58@gmail.com>
2024-05-11 06:55:08 -0300

    Refactor homescreen

--- lib/pages/HomeScreen/home_screen.dart
+++ lib/pages/HomeScreen/home_screen.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
     double myHeight = MediaQuery.of(context).size.height;
     double myWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-       appBar: GFAppBar(
+      appBar: GFAppBar(
         automaticallyImplyLeading: false,
         centerTitle: true,
         backgroundColor: AppTheme.primaryColor,
@@ -52,25 +52,29 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemCount: vacancies.length,
         itemBuilder: (context, index) {
-          return Card(
-            color: CardColor.primaryColor,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              title: Text(
-                vacancies[index].nome,
-                style: TextStyle(color: FontTextColor.primaryColor),
+          return SizedBox(
+            height: myHeight,
+            width: myWidth,
+            child: Card(
+              color: CardColor.primaryColor,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                title: Text(
+                  vacancies[index].nome,
+                  style: TextStyle(color: FontTextColor.primaryColor),
+                ),
+                subtitle: Text(
+                  'ID: ${vacancies[index].id}',
+                  style: TextStyle(color: FontTextColor.primaryColor),
+                ),
+                trailing: Icon(
+                  vacancies[index].ocupada ? Icons.check_circle : Icons.cancel,
+                  color: vacancies[index].ocupada ? Colors.green : Colors.red,
+                ),
+                onTap: () {
+                  // Implement onTap functionality if needed
+                },
               ),
-              subtitle: Text(
-                'ID: ${vacancies[index].id}',
-                style: TextStyle(color: FontTextColor.primaryColor),
-              ),
-              trailing: Icon(
-                vacancies[index].ocupada ? Icons.check_circle : Icons.cancel,
-                color: vacancies[index].ocupada ? Colors.green : Colors.red,
-              ),
-              onTap: () {
-                // Implement onTap functionality if needed
-              },
             ),
           );
         },
